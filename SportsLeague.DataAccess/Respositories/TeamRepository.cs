4ed90a39f5db2e3a05bc6298064a6b9d05cb03b8
YoinerOlaya14 <yoinerolaya14@gmail.com>
2026-03-05 20:07:10 -0500

    Add project files.

---
+++ SportsLeague.DataAccess/Respositories/TeamRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsLeague.DataAccess.Context;
+using SportsLeague.Domain.Entities;
+using SportsLeague.Domain.Interfaces.Repositories;
+
+
+namespace SportsLeague.DataAccess.Respositories
+{
+    public class TeamRepository : GenericRepository<Team>, ITeamRepository
+    {
+        public TeamRepository(LeagueDbContext context) : base(context)
+        {
+        }
+
+
+        //Aqui se devuelve un objeto de tipo Team o null si no se encuentra ningún equipo con el nombre especificado.
+        public async Task<Team?> GetByNameAsync(string name) 
+        {
+            return await _dbSet
+                .FirstOrDefaultAsync(t => t.Name.ToLower() == name.ToLower());
+        }
+
+
+        //Aqui se devuelve una lista de objetos de tipo Team que pertenecen a la ciudad especificada.
+        //Si no se encuentra ningún equipo en esa ciudad, se devuelve una lista vacía.
+        public async Task<IEnumerable<Team>> GetByCityAsync(string city) 
+        {
+            return await _dbSet
+                .Where(t => t.City.ToLower() == city.ToLower())
+                .ToListAsync();
+        }
+    }
+
+}
